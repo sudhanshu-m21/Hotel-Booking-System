1b3dfc358b501ae6785f8db561da5adf4229ef8e
sudhanshu-m21 <sudhanshumishra106@gmail.com>
2025-12-21 16:46:11 +0530

    corrected api

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,42 @@
+// import express from "express";
+// import "dotenv/config";
+// import cors from "cors";
+// import connectDB from "./config/db.js";
+// import { clerkMiddleware } from "@clerk/express";
+// import clerkWebhooks from "./controllers/clerkWebhooks.js";
+// connectDB();
+// const app = express();
+// app.use(cors());
+
+// app.use(express.json());
+// app.use(clerkMiddleware());
+
+// app.use("/api/clerk", clerkWebhooks);
+
+// app.get("/", (req, res) => res.send("API is Working"));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 import express from "express";
-import "dotenv/config";
 import cors from "cors";
+import "dotenv/config";
+
 import connectDB from "./config/db.js";
-import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
+
 connectDB();
+
 const app = express();
 app.use(cors());
 
-app.use(express.json());
-app.use(clerkMiddleware());
+app.post(
+  "/api/clerk/webhook",
+  express.raw({ type: "application/json" }),
+  clerkWebhooks
+);
 
-app.use("/api/clerk", clerkWebhooks);
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("API is Working"));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
