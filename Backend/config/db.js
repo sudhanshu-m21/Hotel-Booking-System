a2bb829ba69933f679c770816cfd0af737b9ad1e
sudhanshu-m21 <sudhanshumishra106@gmail.com>
2025-12-21 17:18:39 +0530

    db corrected

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/hotel_booking`);
+    await mongoose.connect(`${process.env.MONGODB_URI}hotel_booking`);
     console.log("Connected to DB");
   } catch (error) {
     console.log(error.message);
