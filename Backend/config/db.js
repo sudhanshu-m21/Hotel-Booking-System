7ac69db3c77245d13c93378a6aed58ae915bc4e4
sudhanshu-m21 <sudhanshumishra106@gmail.com>
2025-12-21 15:33:42 +0530

    Adding Frontend and Backend

---
+++ Backend/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`);
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default connectDB;
