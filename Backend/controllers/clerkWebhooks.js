3dc33626f4418b771519f319bd91346605e251a1
sudhanshu-m21 <sudhanshumishra106@gmail.com>
2025-12-24 15:22:52 +0530

    corrected some hotel reg error

--- Backend/controllers/clerkWebhooks.js
+++ Backend/controllers/clerkWebhooks.js
@@ -13,25 +13,18 @@ const clerkWebhooks = async (req, res) => {
     await whook.verify(JSON.stringify(req.body), headers);
 
     const { data, type } = req.body;
-
+    const userData = {
+      _id: data.id,
+      email: data.email_addresses[0].email_address,
+      username: data.first_name + " " + data.last_name,
+      image: data.image_url,
+    };
     switch (type) {
       case "user.created": {
-        const userData = {
-          _id: data.id,
-          email: data.email_addresses[0].email_address,
-          username: data.first_name + " " + data.last_name,
-          image: data.image_url,
-        };
         await User.create(userData);
         break;
       }
       case "user.updated": {
-        const userData = {
-          _id: data.id,
-          email: data.email_addresses[0].email_address,
-          username: data.first_name + " " + data.last_name,
-          image: data.image_url,
-        };
         await User.findByIdAndUpdate(data.id, userData);
         break;
       }
@@ -44,7 +37,6 @@ const clerkWebhooks = async (req, res) => {
     }
     res.json({ success: true, message: "Webhook Recieved" });
   } catch (error) {
-    console.log(error.message);
     res.json({ success: false, message: error.message });
   }
 };
